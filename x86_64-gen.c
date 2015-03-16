2cbb52c2adc1e5ddba1f8f4329ddc66f30f07d48
Philip <pipcet@gmail.com>
2015-03-16 12:09:42 +0000

    rex prefix for lea and similar instructions using the SIB

--- x86_64-gen.c
+++ x86_64-gen.c
@@ -554,6 +554,18 @@ void orex(int ll, int r, int r2, int b)
     o(b);
 }
 
+/* always emit rex prefix. */
+void orex4(int ll, int r3, int r, int r2, int b)
+{
+    ib();
+    if ((r & VT_VALMASK) >= VT_CONST)
+        r = 0;
+    if ((r2 & VT_VALMASK) >= VT_CONST)
+        r2 = 0;
+    o(0x40 | REX_BASE(r) | (REX_BASE(r2) << 2) | (ll << 3) | (REX_BASE(r3)<<1));
+    o(b);
+}
+
 /* output a symbol and patch all calls to it */
 int gsym_addr(int t, int a)
 {
